b6dd597544fbaa61c660affd536f214c8f2659a2
Daniel Li <x711Li@gmail.com>
2011-10-09 05:18:38 -0300

    Removed environment variables.

--- launch_login.bat
+++ launch_login.bat
@@ -1,5 +1,5 @@
 @echo off
 @title Login Server
-set CLASSPATH=.;dist\v88.jar;dist\mina-core.jar;dist\slf4j-api.jar;dist\slf4j-jdk14.jar;dist\mysql-connector-java-bin.jar
-"C:\Program Files\Java\jdk1.6.0_24\bin\java.exe" -Xmx300m -Dwzpath=wz\ -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd -Drecvops=recvops.properties -Dsendops=sendops.properties net.login.LoginServer
+set CLASSPATH=.;dist\odinms.jar;dist\mina-core.jar;dist\slf4j-api.jar;dist\slf4j-jdk14.jar;dist\mysql-connector-java-bin.jar
+java -Xmx300m -Dwzpath=wz\ -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd -Drecvops=recvops.properties -Dsendops=sendops.properties net.login.LoginServer
 pause
