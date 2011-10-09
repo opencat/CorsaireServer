67ea38e5ee25cadcfae43876b8e57240c89490f8
Daniel Li <x711Li@gmail.com>
2011-10-09 05:19:04 -0300

    Removed environment variables.

--- launch_world.bat
+++ launch_world.bat
@@ -1,5 +1,5 @@
 @echo off
 @title World Server
-set CLASSPATH=.;dist\v88.jar;dist\mina-core.jar;dist\slf4j-api.jar;dist\slf4j-jdk14.jar;dist\mysql-connector-java-bin.jar
-"C:\Program Files\Java\jdk1.6.0_24\bin\java.exe" -Xmx300m -Dwzpath=wz\ -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd -Drecvops=recvops.properties -Dsendops=sendops.properties net.world.WorldServer
+set CLASSPATH=.;dist\odinms.jar;dist\mina-core.jar;dist\slf4j-api.jar;dist\slf4j-jdk14.jar;dist\mysql-connector-java-bin.jar
+java -Xmx300m -Dwzpath=wz\ -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd -Drecvops=recvops.properties -Dsendops=sendops.properties net.world.WorldServer
 pause
